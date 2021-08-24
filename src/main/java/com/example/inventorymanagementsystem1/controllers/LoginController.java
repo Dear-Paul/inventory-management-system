b82476aa6509df6554f8dfab026cc1c522fbf35f
Innocent-Ogesiano <innocentekweogu@gmail.com>
2021-08-24 11:04:51 +0100

    Admin book section

--- src/main/java/com/example/inventorymanagementsystem1/controllers/LoginController.java
+++ src/main/java/com/example/inventorymanagementsystem1/controllers/LoginController.java
@@ -6,4 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
+    @GetMapping("/")
+    public String homePage() {
+        System.out.println("in home page");
+        return "index";
+    }
+
 }
--- src/main/java/com/example/inventorymanagementsystem1/services/AdminServices.java
+++ src/main/java/com/example/inventorymanagementsystem1/services/AdminServices.java
@@ -1,4 +1,14 @@
 package com.example.inventorymanagementsystem1.services;
 
+import com.example.inventorymanagementsystem1.model.Book;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface AdminServices {
+    List<Book> listOfBooks ();
+    Book getBookById (long id);
+    Book addBookToStore (Book book);
+    void removeBookFromStore (long id);
 }
